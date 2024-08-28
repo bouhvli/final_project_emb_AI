23220555898dae602ff7cd05fb05569c3ad8d17f
bouhvli <bouhali.hamza.2@gmail.com>
2024-08-28 09:51:21 -0400

    error handling and pylint

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,11 +6,20 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json=myobj, headers=header)
-    response_in_json = json.loads(response.text)
+    if response.status_code == 400:
+        output_res = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+        return output_res
 
+    response_in_json = json.loads(response.text)
     emotions = response_in_json['emotionPredictions'][0]['emotion']
     dominant_emotion = max(emotions, key=emotions.get)
-
     output_res = {
         'anger': response_in_json['emotionPredictions'][0]['emotion']['anger'],
         'disgust': response_in_json['emotionPredictions'][0]['emotion']['disgust'],
--- server.py
+++ server.py
@@ -1,14 +1,25 @@
+''' Executing this function initiates the application of emotional
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
 from flask import Flask, request, render_template
 from EmotionDetection import emotion_detector
 
+# Import the sentiment_analyzer function from the package created
 app = Flask('Emotion Detector')
 
 @app.route('/emotionDetector')
 def emotion_analyser():
+    ''' This code receives the text from the HTML interface and 
+        runs emotional detection over it using emotion_detector()
+        function. The output returns a dict of emotions and the dominant emotion.
+    '''
     text_to_analyse = request.args.get('textToAnalyze')
     results_of_analysis = emotion_detector(text_to_analyse)
-    if results_of_analysis is None:
-        return "Invalid input! Try again."
+
+    if results_of_analysis['dominant_emotion'] is None:
+        return "Invalid text! Please try again!."
+
     return f"For the given statement, the system response is\
      'anger': {results_of_analysis['anger']}, 'disgust': {results_of_analysis['disgust']},\
       'fear': {results_of_analysis['fear']}, 'joy': {results_of_analysis['joy']}\
@@ -17,6 +28,9 @@ def emotion_analyser():
 
 @app.route("/")
 def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
