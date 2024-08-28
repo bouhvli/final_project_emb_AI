3e522ba79dc57255bca091f71aa2acaab08992de
bouhvli <bouhali.hamza.2@gmail.com>
2024-08-28 08:31:18 -0400

    first commit

---
+++ emotion_detection.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json=myobj, headers=header)
+    response_in_json = json.loads(response.text)
+
+    emotions = response_in_json['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    output_res = {
+        'anger': response_in_json['emotionPredictions'][0]['emotion']['anger'],
+        'disgust': response_in_json['emotionPredictions'][0]['emotion']['disgust'],
+        'fear': response_in_json['emotionPredictions'][0]['emotion']['fear'],
+        'joy': response_in_json['emotionPredictions'][0]['emotion']['joy'],
+        'sadness': response_in_json['emotionPredictions'][0]['emotion']['sadness'],
+        'dominant_emotion': dominant_emotion
+    }
+    return output_res
